a088cfda50e24f53b5370f50e29395deddb56a0b
Wycliff <w4wycliff@gmail.com>
2022-05-24 10:17:00 +0200

    Ecommerce frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Cart from "./pages/Cart";
+import Home from "./pages/Home"
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+import styled from "styled-components";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+	display: flex;
+	${mobile({ flexDirection: "column" })}
+`;
+
+const Left = styled.div`
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+	margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+	display: flex;
+`;
+
+const SocialIcon = styled.div`
+	width: 40px;
+	height: 40px;
+	border-radius: 50%;
+	color: white;
+	background-color: #${(props) => props.color};
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-right: 20px;
+`;
+
+const Center = styled.div`
+	flex: 1;
+	padding: 20px;
+	${mobile({ display: "none" })}
+`;
+
+const Title = styled.h3`
+	margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+	margin: 0;
+	padding: 0;
+	list-style: none;
+	display: flex;
+	flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+	width: 50%;
+	margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+	flex: 1;
+	padding: 20px;
+	${mobile({ backgroundColor: "#fff8f8" })}
+`;
+
+const ContactItem = styled.div`
+	margin-bottom: 20px;
+	display: flex;
+	align-items: center;
+`;
+
+const Payment = styled.img`
+	width: 50%;
+`;
+
+const Footer = () => {
+	return (
+		<Container>
+			<Left>
+				<Logo>Wyco.</Logo>
+				<Desc>
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry. Lorem Ipsum has been the industry's standard dummy text ever
+					since the 1500s, when an unknown printer took a galley of type and
+					scrambled it to make a type specimen book. It has survived not only
+					five centuries, but also the leap into electronic typesetting,
+					remaining essentially unchanged.
+				</Desc>
+				<SocialContainer>
+					<SocialIcon color="#3B5999">
+						<FacebookIcon />
+					</SocialIcon>
+
+					<SocialIcon color="#E4405F">
+						<InstagramIcon />
+					</SocialIcon>
+
+					<SocialIcon color="#55ACEE">
+						<TwitterIcon />
+					</SocialIcon>
+
+					<SocialIcon color="#E60023">
+						<PinterestIcon />
+					</SocialIcon>
+				</SocialContainer>
+			</Left>
+			<Center>
+				<Title>Useful Links</Title>
+				<List>
+					<ListItem>Home</ListItem>
+					<ListItem>Cart</ListItem>
+					<ListItem>Man Fashion</ListItem>
+					<ListItem>Woman Fashion</ListItem>
+					<ListItem>Accessories</ListItem>
+					<ListItem>My Account</ListItem>
+					<ListItem>Order Tracking</ListItem>
+					<ListItem>Wishlist</ListItem>
+					<ListItem>Terms</ListItem>
+				</List>
+			</Center>
+			<Right>
+				<Title>Contact</Title>
+				<ContactItem>
+					<LocationOnIcon style={{ marginRight: "10px" }} /> 103 rue de Marseil
+				</ContactItem>
+				<ContactItem>
+					<PhoneIcon style={{ marginRight: "10px" }} /> +33-060-188-666
+				</ContactItem>
+				<ContactItem>
+					<EmailIcon style={{ marginRight: "10px" }} /> info@wycliff.dev
+				</ContactItem>
+
+				<Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+			</Right>
+		</Container>
+	);
+};
+
+export default Footer;
